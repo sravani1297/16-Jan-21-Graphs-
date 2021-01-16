a640af9c48afb2007760d11b1e29590b33f3d5c9
sravani1297 <72606270+sravani1297@users.noreply.github.com>
2021-01-16 21:00:41 +0530

    Add files via upload

---
+++ problem2.java
@@ -0,0 +1,24 @@
+class Solution
+{
+    private ArrayList<Integer> r = new ArrayList<Integer>();
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj)
+    {   boolean printed[] = new boolean[V];
+        LinkedList<Integer> q = new LinkedList<Integer>();
+        printed[0]=true;
+        q.add(0);
+        while (q.size() != 0)
+        {   
+            int c = q.removeFirst();
+            r.add(c);
+            for(Integer j:adj.get(c))
+            {
+                if (!printed[j])
+                {
+                    printed[j] = true;
+                    q.add(j);
+                }
+            }
+        }
+        return r;
+    }
+}
