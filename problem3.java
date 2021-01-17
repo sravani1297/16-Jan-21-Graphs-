0f33bafb73c47a0ab966f83b387b92334b272539
sravani1297 <72606270+sravani1297@users.noreply.github.com>
2021-01-17 20:31:58 +0530

    Add files via upload

---
+++ problem3.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static class Edge
+	{
+        // Assigning values to src,nbr,wt
+    }
+    public static void main(String[] args) throws Exception {
+        // Reading input from the user
+        // write your code here
+        boolean visited[] = new boolean[vtces];
+        boolean res=dfsOfGraph(src,dest,graph,visited);
+        System.out.println(res);
+    }
+    public static boolean dfsOfGraph(int src,int dest,ArrayList < Edge > [] graph,boolean[] visited)
+    {
+        if (src==dest)
+            return true;
+        visited[src] = true;
+        boolean result = false;
+        for(Edge e: graph[src])
+        {
+            if(!visited[e.nbr])
+                result= dfsOfGraph(e.nbr,dest,graph,visited);
+            if (result)
+                return result;
+        }
+        return false;
+    }
+}
